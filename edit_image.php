edfcbb1aa339e536f9cda675a8b43a8f76f66743
JatinMaharjan <jatinm99999@gmail.com>
2022-05-11 13:08:48 +0545

    commit

--- edit_image.php
+++ edit_image.php
@@ -1,26 +1,26 @@
 <div class="container mt-4">
 <?php
-if(isset($_GET['edit_contact'])) {
-    $the_contact_id = $_GET['edit_contact'];
+if(isset($_GET['edit_image'])) {
+    $the_image_id = $_GET['edit_image'];
 
-    $query = "SELECT * FROM contacts where contact_id = $the_contact_id ";
-    $sel_contacts_query = mysqli_query($connection, $query);
+    $query = "SELECT * FROM images where image_id = $the_image_id ";
+    $sel_images_query = mysqli_query($connection, $query);
 
-    while($row = mysqli_fetch_assoc($sel_contacts_query)) {
-        $contact_id = $row['contact_id'];
-        $contact_no = $row['contact_no'];
-        $contact_document = $row['contact_document'];
+    while($row = mysqli_fetch_assoc($sel_images_query)) {
+        $image_id = $row['image_id'];
+        $image_no = $row['image_no'];
+        $image_document = $row['image_document'];
     }
 
 }
 
 
-if(isset($_POST['edit_contact'])) {
-    $contact_no = $_POST['contact'];
-    $contact_document = $_FILES['document']['name'];
-    $contact_document_temp = $_FILES['document']['tmp_name'];
+if(isset($_POST['edit_image'])) {
+    $image_no = $_POST['image'];
+    $image_document = $_FILES['document']['name'];
+    $image_document_temp = $_FILES['document']['tmp_name'];
 
-    move_uploaded_file($contact_document_temp, "documents/$contact_document");
+    move_uploaded_file($image_document_temp, "documents/$image_document");
 
     $allowed_extension = array('jpeg', 'jpg', 'pdf', 'doc', 'docx');
     $filename = $_FILES['document']['name'];
@@ -31,15 +31,15 @@ if(isset($_POST['edit_contact'])) {
     }
     else{
 
-    $query = "UPDATE contacts SET ";
-    $query .="contact_no = '{$contact_no}', ";
-    $query .="contact_document = '{$contact_document}' ";
-    $query .="WHERE contact_id = {$the_contact_id} ";
+    $query = "UPDATE images SET ";
+    $query .="image_no = '{$image_no}', ";
+    $query .="image_document = '{$image_document}' ";
+    $query .="WHERE image_id = {$the_image_id} ";
 
-    $log_action="Contact updated";
+    $log_action="Image updated";
     create_log($_SESSION['username'], $_SESSION['user_id'], $log_action);
-    $update_contact_query = mysqli_query($connection, $query);
-    confirm($update_contact_query);
+    $update_image_query = mysqli_query($connection, $query);
+    confirm($update_image_query);
     }
 }
 ?>
@@ -48,20 +48,17 @@ if(isset($_POST['edit_contact'])) {
 
 <form action="" method="post" enctype="multipart/form-data"> 
 <div class="row ">
-  <div class="col mb-3">
-    <label for="inputContact">Contact number</label>
-    <input type="text" class="form-control" value="<?php echo $contact_no ?>" id="inputContacts" name="contact" placeholder="980******">
-  </div>
+  
     <div class="col mb-3">
     <label for="inputFile">Upload file</label>
     <input type="file" class="form-control"  
     id="inputFiles" name="document">
   </div>
   <div class="col mb-3">
-  <input type="submit" name="edit_contact" class="btn btn-primary mt-4" value="Update">
+  <input type="submit" name="edit_image" class="btn btn-primary mt-4" value="Update">
   </div>
    <div class="col mb-3 p-5">
-      <p><a href="documents/<?php echo $contact_document?>"><?php echo $contact_document?></a></p>
+      <p><a href="documents/<?php echo $image_document?>"><?php echo $image_document?></a></p>
   </div>
   </div>
 </form>
